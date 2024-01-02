0bcc40129cadcacb93ec91ddee86e550dca77b65
Alireza <ali1985reza@gmail.com>
2024-01-02 15:46:13 +1100

    complete calculator and refactoring if conditions

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
 import tkinter as tk
 
-windows = tk.Tk()
-windows.title('My Calculator')
-
-lbl_calc_result = tk.Label(
-    master=windows,
-    text='0',
-    width=30,
-    height=3
-)
-
-lbl_calc_result.grid(row=0, column=0, columnspan=4)
-
-
-def insert_number_in_calc_result(btn_text):
-    current = lbl_calc_result['text']
-    if current == '0':
-        lbl_calc_result['text'] = btn_text
-    elif btn_text == '=':
-        lbl_calc_result['text'] = f"{eval(current)}"
-    else:
-        if btn_text in ['+', '-', '*']:
-            if current[-1] in ['+', '-', '*']:
-                lbl_calc_result['text'] = current[:-1] + btn_text
-            else:
-                lbl_calc_result['text'] += btn_text
-        else:
-            lbl_calc_result['text'] += btn_text
-
 
 cals_keys = [
     {
@@ -96,8 +68,53 @@ cals_keys = [
     },
 ]
 
-calc_keys_objs = []
 
+windows = tk.Tk()
+windows.title('My Calculator')
+
+lbl_calc_result = tk.Label(
+    master=windows,
+    text='0',
+    width=30,
+    height=3
+)
+
+lbl_calc_result.grid(row=0, column=0, columnspan=4)
+
+last_dot_index = -1
+last_op_index = -1
+
+
+def insert_number_in_calc_result(btn_text):
+    current = lbl_calc_result['text']
+
+    global last_op_index, last_dot_index
+    if btn_text in ['+', '-', '*']:
+        last_op_index = len(current)
+
+    if btn_text == 'C':
+        lbl_calc_result['text'] = '0'
+        last_op_index = 0
+        last_dot_index = 0
+    elif current == '0':
+        lbl_calc_result['text'] = btn_text
+    elif btn_text == '=':
+        result = f"{eval(current)}"
+        lbl_calc_result['text'] = result
+        last_op_index = 0,
+        last_dot_index = 0
+        if '.' in result:
+            last_dot_index = result.index('.')
+    elif btn_text == '.' and not (last_dot_index > last_op_index or current[-1] == '.'):
+        lbl_calc_result['text'] += btn_text
+        last_dot_index = len(current)
+    elif btn_text in ['+', '-', '*'] and current[-1] in ['+', '-', '*']:
+        lbl_calc_result['text'] = current[:-1] + btn_text
+    else:
+        lbl_calc_result['text'] += btn_text
+
+
+calc_keys_objs = []
 for calc_key_data in cals_keys:
     btn = tk.Button(
         master=windows,
