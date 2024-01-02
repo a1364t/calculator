74d6c173999aace8574c0dc16407f727eb2d416e
Alireza <ali1985reza@gmail.com>
2024-01-02 11:32:53 +1100

    button 7, 8, 9, +

--- main.py
+++ main.py
@@ -9,6 +9,38 @@ lbl_calc_result = tk.Label(
     height=3
 )
 
-lbl_calc_result.grid(row=0, column=0)
+lbl_calc_result.grid(row=0, column=0, columnspan=4)
+
+btn_7 = tk.Button(
+    master=windows,
+    text='7',
+    command=lambda: print('7'),
+    height=3
+)
+btn_7.grid(row=1, column=0, sticky='nsew')
+
+btn_8 = tk.Button(
+    master=windows,
+    text='8',
+    command=lambda: print('8'),
+    height=3,
+)
+btn_8.grid(row=1, column=1, sticky='nsew')
+
+btn_9 = tk.Button(
+    master=windows,
+    text='9',
+    command=lambda: print('9'),
+    height=3,
+)
+btn_9.grid(row=1, column=2, sticky='nsew')
+
+btn_plus = tk.Button(
+    master=windows,
+    text='+',
+    command=lambda: print('+'),
+    height=3,
+)
+btn_plus.grid(row=1, column=3, sticky='nsew')
 
 windows.mainloop()
