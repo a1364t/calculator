6e3d86288ad67c2c0e74ed4dd715047ac6db91e6
Alireza <ali1985reza@gmail.com>
2024-01-02 14:57:01 +1100

    connect buttons to variabls and control the conditions

--- main.py
+++ main.py
@@ -12,70 +12,87 @@ lbl_calc_result = tk.Label(
 
 lbl_calc_result.grid(row=0, column=0, columnspan=4)
 
+
+def insert_number_in_calc_result(btn_text):
+    current = lbl_calc_result['text']
+    if current == '0':
+        lbl_calc_result['text'] = btn_text
+    elif btn_text == '=':
+        lbl_calc_result['text'] = f"{eval(current)}"
+    else:
+        if btn_text == '+' or btn_text == '-' or btn_text == '*':
+            if current[-1] == '+' or current[-1] == '-' or current[-1] == '*':
+                lbl_calc_result['text'] = current[:-1] + btn_text
+            else:
+                lbl_calc_result['text'] += btn_text
+        else:
+            lbl_calc_result['text'] += btn_text
+
+
 cals_keys = [
     {
         'text': '7',
-        'command': lambda: print('7'),
+        'command': lambda: insert_number_in_calc_result('7'),
     },
     {
         'text': '8',
-        'command': lambda: print('8'),
+        'command': lambda: insert_number_in_calc_result('8'),
     },
     {
         'text': '9',
-        'command': lambda: print('9'),
+        'command': lambda: insert_number_in_calc_result('9'),
     },
     {
         'text': '+',
-        'command': lambda: print('+'),
+        'command': lambda: insert_number_in_calc_result('+'),
     },
     {
         'text': '4',
-        'command': lambda: print('4'),
+        'command': lambda: insert_number_in_calc_result('4'),
     },
     {
         'text': '5',
-        'command': lambda: print('5'),
+        'command': lambda: insert_number_in_calc_result('5'),
     },
     {
         'text': '6',
-        'command': lambda: print('6'),
+        'command': lambda: insert_number_in_calc_result('6'),
     },
     {
         'text': '-',
-        'command': lambda: print('-'),
+        'command': lambda: insert_number_in_calc_result('-'),
     },
     {
         'text': '1',
-        'command': lambda: print('1'),
+        'command': lambda: insert_number_in_calc_result('1'),
     },
     {
         'text': '2',
-        'command': lambda: print('2'),
+        'command': lambda: insert_number_in_calc_result('2'),
     },
     {
         'text': '3',
-        'command': lambda: print('3'),
+        'command': lambda: insert_number_in_calc_result('3'),
     },
     {
         'text': '*',
-        'command': lambda: print('*'),
+        'command': lambda: insert_number_in_calc_result('*'),
     },
     {
         'text': '.',
-        'command': lambda: print('.'),
+        'command': lambda: insert_number_in_calc_result('.'),
     },
     {
         'text': '0',
-        'command': lambda: print('0'),
+        'command': lambda: insert_number_in_calc_result('0'),
     },
     {
         'text': 'C',
-        'command': lambda: print('C'),
+        'command': lambda: insert_number_in_calc_result('C'),
     },
     {
         'text': '=',
-        'command': lambda: print('='),
+        'command': lambda: insert_number_in_calc_result('='),
     },
 ]
 
@@ -93,37 +110,5 @@ for calc_key_data in cals_keys:
 for i, calc_key_obj in enumerate(calc_keys_objs):
     calc_key_obj.grid(row=(i//4)+1, column=i % 4, sticky='nsew')
 
-
-# btn_7 = tk.Button(
-#     master=windows,
-#     text='7',
-#     command=lambda: print('7'),
-#     height=3
-# )
-# btn_7.grid(row=1, column=0, sticky='nsew')
-
-# btn_8 = tk.Button(
-#     master=windows,
-#     text='8',
-#     command=lambda: print('8'),
-#     height=3,
-# )
-# btn_8.grid(row=1, column=1, sticky='nsew')
-
-# btn_9 = tk.Button(
-#     master=windows,
-#     text='9',
-#     command=lambda: print('9'),
-#     height=3,
-# )
-# btn_9.grid(row=1, column=2, sticky='nsew')
-
-# btn_plus = tk.Button(
-#     master=windows,
-#     text='+',
-#     command=lambda: print('+'),
-#     height=3,
-# )
-# btn_plus.grid(row=1, column=3, sticky='nsew')
 
 windows.mainloop()
